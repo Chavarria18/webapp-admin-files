73abe830cc31486ce92390eb968ea80d8c6f01b3
Gabriel Chavarria <gchavarriamunoz@gmail.com>
2026-09-19 12:40:19 -0600

    feat: home table created by

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -62,7 +62,7 @@
                                             <i class="bi bi-copy"></i>
                                         </button>
                                     </td>
-                                    <td>{{ $file->name }}</td>
+                                    <td>{{ $file->email }}</td>
 
                                     <td>
                                         {{ number_format($file->size / 1024, 2) }} KB

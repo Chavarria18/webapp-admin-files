f5583205847953f86fbe86b7d739f741690012a6
Gabriel Chavarria <gchavarriamunoz@gmail.com>
2026-09-20 20:28:44 -0600

    fix: task adding  0 days param

--- app/Console/Commands/DeleteExpiredFiles.php
+++ app/Console/Commands/DeleteExpiredFiles.php
@@ -8,7 +8,7 @@ use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
 use App\Services\S3Service;
 
-#[Signature('files:delete-expired')]
+#[Signature('files:delete-expired {--days=30}')]
 #[Description('Command description')]
 class DeleteExpiredFiles extends Command
 {
@@ -17,17 +17,27 @@ class DeleteExpiredFiles extends Command
      */
     public function handle(S3Service $s3)
     {
-        $this->info('Expired files deleted.');
-        $files = File::onlyTrashed()
-            ->where('deleted_at', '<', now()->subDays(30))
-            ->get();
+
+        $days = $this->option('days');
+        $this->info('Eliminando archivos de ' . $days . ' dias de antiguedad');
+
+        if ($days == 0) {
+            $files = File::onlyTrashed()
+
+                ->get();
+        } else {
+            $files = File::onlyTrashed()
+                ->where('deleted_at', '<', now()->subDays($days))
+                ->get();
+        }
+
 
         foreach ($files as $file) {
             $s3->deleteIfExists($file->s3dir);
             $file->forceDelete();
-            $this->info("Deleted: {$file->name}");
+            $this->info("Eliminado: {$file->name}");
         }
-
+        $this->info('Archivos en basurero eliminados');
 
         return Command::SUCCESS;
     }
